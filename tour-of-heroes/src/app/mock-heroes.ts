bc75775cb25817a42510cd5e358a99a7705a2c62
Rolfe <34160154+DarrenRolfe@users.noreply.github.com>
2018-01-26 08:25:06 +0000

    Tour of Heroes
    Subscription and hero-detail

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './Hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Superman' },  
+  { id: 12, name: 'Wonder Woman' },  
+  { id: 13, name: 'Black Canary' },  
+  { id: 14, name: 'Mr Fantastic' },  
+  { id: 15, name: 'Green Arrow' },  
+  { id: 16, name: 'Firestorm' },  
+  { id: 17, name: 'Atom' },  
+  { id: 18, name: 'The Flash' },  
+  { id: 19, name: 'Vibe' },  
+  { id: 20, name: 'Gypsy' },  
+];
